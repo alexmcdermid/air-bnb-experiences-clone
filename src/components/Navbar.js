d0038091a646f73c157fd646621ea1e75ea58217
alexmcdermid <alexander.mcdermid@gmail.com>
2022-02-09 14:59:50 -0800

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+  <nav><img src={require("../images/airbnb-logo.png")} className="nav--logo" alt="" /></nav>
+  )
+}
