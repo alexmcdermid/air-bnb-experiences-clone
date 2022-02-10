00411686add5e7df629e49bd2e22037f79fc09ac
alexmcdermid <alexander.mcdermid@gmail.com>
2022-02-10 12:14:04 -0800

    use map to load data into cards

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
 import Card from './components/Card/Card';
+import cardData from './data.js';
 
 function App() {
+  const cards = cardData.map(card=>{
+    return(
+      <Card
+        img={card.coverImg}
+        rating={card.stats.rating}
+        ratings={card.stats.reviewCount}
+        country={card.location}
+        bio={card.title}
+        price={card.price}
+      />
+    )
+  });
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
-      <Card 
-      img="./images/katie-zaferes.png"
-      rating="5.0"
-      ratings="6"
-      country="USA"
-      bio="Life lessons with Katie Zaferes"
-      rate="136"
-      />
+      {cards}
+      
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,15 +4,15 @@ import "./Card.css";
 export default function Card(props) {
   return (
       <div className='card'>
-          <img src={require("../../images/katie-zaferes.png")} className="card--image" alt=""></img>
+          <img src={require(`../../images/${props.img}`)} className="card--image" alt=""></img>
           <div className='card--stats'>
             <img src={require("../../images/star.png")} className="card--star" alt=""/>
             <span>{props.rating}</span>
             <span className='gray'>({props.ratings}) • </span>
             <span className='gray'>{props.country}</span>
           </div>
-          <p>{props.bio}</p>
-          <p><span className='bold'>From ${props.rate}</span> / person</p>
+          <p className='card--p'>{props.bio}</p>
+          <p className='=card--p'><span className='bold'>From ${props.price}</span> / person</p>
       </div>
   )
 }
