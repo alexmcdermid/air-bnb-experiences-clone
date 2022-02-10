0388a5ad0a09b3682904628e11b45c96bc83a80c
alexmcdermid <alexander.mcdermid@gmail.com>
2022-02-10 12:54:33 -0800

    add online badge for online users

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,15 @@ import React from 'react';
 import "./Card.css";
 
 export default function Card(props) {
-  console.log(props.openSpots);
+  let badgeText
+  if (props.openSpots === 0) {
+      badgeText = "SOLD OUT"
+  } else if (props.country === "Online") {
+      badgeText = "ONLINE"
+  }
   return (
       <div className='card'>
-          {props.openSpots === 0 && <div className='card--badge'>SOLD OUT</div>}
+          {badgeText && <div className='card--badge'>{badgeText}</div>}
           <img src={require(`../../images/${props.img}`)} className="card--image" alt=""></img>
           <div className='card--stats'>
             <img src={require("../../images/star.png")} className="card--star" alt=""/>
